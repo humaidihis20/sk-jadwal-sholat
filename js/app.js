a6f234569a8e1ddfe45e2ad8fb3d3f86701850c2
Hisyam <hisamhumaidi@gmail.com>
2021-11-22 07:15:17 +0700

    geolocation permision

--- js/app.js
+++ js/app.js
@@ -1,7 +1,29 @@
+//
+function success(position) {
+  console.log(position);
+}
+
+function error() {
+  alert("Posisi tidak dapat di akses");
+}
+
+//
+function userLocation() {
+  if (!navigator.geolocation) {
+    alert("Geolocation tidak didukung di browser anda, silahkan gunakan browser lain");
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
+
+//
 function index() {
   let app = document.getElementById("app");
   let h3 = document.createElement("h3");
   h3.textContent = "Prayer Times";
+
   app.appendChild(h3);
+
+  userLocation();
 }
 index();
