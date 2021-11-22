c24a07b42e43da0a192ee49258123f36af3ee731
Hisyam <hisamhumaidi@gmail.com>
2021-11-22 07:51:11 +0700

    mendapatkan jadwal sholat hari ini

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
-//
-function success(position) {
-  console.log(position);
+// fetch api dari aladhan.com
+function prayerTimes(latitude, longitude) {
+  fetch("http://api.aladhan.com/v1/calendar?latitude=" + latitude + "&longitude=" + longitude + "&method=2")
+    .then((response) => response.json())
+    .then(function (response) {
+      let date = new Date();
+      let today = date.getDate() - 1;
+      // console.log(today);
+      // console.log(response.data[today]);
+    });
 }
 
+// berhasil
+function success(position) {
+  prayerTimes(position.coords.latitude, position.coords.longitude);
+}
+
+// error / kesalahan
 function error() {
   alert("Posisi tidak dapat di akses");
 }
 
-//
+// menampilkan pesan error geolocation
 function userLocation() {
   if (!navigator.geolocation) {
     alert("Geolocation tidak didukung di browser anda, silahkan gunakan browser lain");
@@ -16,7 +29,7 @@ function userLocation() {
   }
 }
 
-//
+// global
 function index() {
   let app = document.getElementById("app");
   let h3 = document.createElement("h3");
